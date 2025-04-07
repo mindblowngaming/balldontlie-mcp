25ebbf691d39e994f24dfd6085ac3bd5cc54cdad
mikechao <mike.chao.one@gmail.com>
2025-04-07 16:12:24 -0700

    Setup first tool get_teams

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import type { Tool } from '@modelcontextprotocol/sdk/types.js';
 import process from 'node:process';
 import { BalldontlieAPI } from '@balldontlie/sdk';
-import { Server } from '@modelcontextprotocol/sdk/server/index.js';
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
+import z from 'zod';
 
-const server = new Server(
+const server = new McpServer(
   {
     name: `Balldontlie MCP Server`,
     version: `0.5.0`,
@@ -25,17 +27,75 @@ if (!BALLDONTLIE_API_KEY) {
 
 const api = new BalldontlieAPI({ apiKey: BALLDONTLIE_API_KEY });
 
+server.tool(
+  'get_teams',
+  'Gets the list of team from one of the following leagues EPL, NBA, MLB, NFL',
+  {
+    league: z.enum(['NBA', 'MLB', 'NFL']),
+  },
+  async ({ league }) => {
+    switch (league) {
+      case 'NBA': {
+        const nbaTeams = await api.nba.getTeams();
+        const text = nbaTeams.data.map((team) => {
+          return `Full Name: ${team.full_name}\n`
+            + `Name: ${team.name}\n`
+            + `Abbreviation: ${team.abbreviation}\n`
+            + `City: ${team.city}\n`
+            + `Conference: ${team.conference}\n`
+            + `Division: ${team.division}\n`;
+        }).join('\n-----\n');
+        return { content: [{ type: 'text', text }] };
+      }
+
+      case 'MLB': {
+        const mlbTeams = await api.mlb.getTeams();
+        const text = mlbTeams.data.map((team) => {
+          return `Display Name: ${team.display_name}\n`
+            + `Name: ${team.name}\n`
+            + `Abbreviation: ${team.abbreviation}\n`
+            + `Location: ${team.location}\n`
+            + `League: ${team.league}\n`
+            + `Division: ${team.division}\n`
+            + `Slug: ${team.slug}\n`;
+        }).join('\n-----\n'); ;
+        return { content: [{ type: 'text', text }] };
+      }
+
+      case 'NFL': {
+        const nlfTeams = await api.nfl.getTeams();
+        const text = nlfTeams.data.map((team) => {
+          return `Name: ${team.name}\n`
+            + `Abbreviation: ${team.abbreviation}\n`
+            + `Full Name: ${team.full_name}\n`
+            + `Location: ${team.location}\n`
+            + `Conference: ${team.conference}\n`
+            + `Division: ${team.division}\n`;
+        }).join('\n-----\n');
+        return { content: [{ type: 'text', text }] };
+      }
+
+      default: {
+        return {
+          content: [{ type: 'text', text: `Unknown league: ${league}` }],
+          isError: true,
+        };
+      }
+    }
+  },
+);
+
 async function main() {
   const transport = new StdioServerTransport();
   await server.connect(transport);
-  log('Server is running with Stdio transport');
+  log('Balldontlie MCP Server is running with Stdio transport');
 }
 
 function log(
   message: string,
   level: 'error' | 'debug' | 'info' | 'notice' | 'warning' | 'critical' | 'alert' | 'emergency' = 'info',
 ) {
-  server.sendLoggingMessage({
+  server.server.sendLoggingMessage({
     level,
     message,
   });
